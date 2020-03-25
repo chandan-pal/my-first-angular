1e23783ae6c0470263670616d945075dce9541c0
chandan-pal <chandanpal290190@gmail.com>
2020-03-25 14:08:52 +0530

    passing input to components

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-address-card',
@@ -6,21 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-card.component.css']
 })
 export class AddressCardComponent implements OnInit {
+  
+  @Input('userName')
+  userName : string;
+  
   user;
 
   constructor() {
+    
+   }
+
+  ngOnInit(): void {
     this.user = {
-      name : "Chandan Pal",
+      name : this.userName,
       title : "Software Developer",
       address : "C1-206, Akshar Elementa Society, Tathawade",
       phone : [
         "123-123-123",
         "456-456-456"
       ]
-    }
-   }
-
-  ngOnInit(): void {
+    };
   }
 
 }
