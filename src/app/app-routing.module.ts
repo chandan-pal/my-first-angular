73d3972fcad7563152f86d9d929a171628f13d4e
chandan-pal <chandanpal290190@gmail.com>
2020-03-29 19:22:44 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { RestappComponent } from './restapp/restapp.component';
+import { DateComponent } from './date/date.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "dateapp", component: DateComponent},
+  {path: "date", redirectTo: "/dateapp", pathMatch: "prefix"},
+  {path: "githubapp", component: RestappComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
