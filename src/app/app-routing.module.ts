d62e26583fea88981ec226e0336922a856805df2
chandan-pal <chandanpal290190@gmail.com>
2020-03-29 19:38:33 +0530

    nesting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { RestappComponent } from './restapp/restapp.component';
 import { DateComponent } from './date/date.component';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './view/header/header.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 const routes: Routes = [
   {path: "dateapp", component: DateComponent},
   {path: "date", redirectTo: "/dateapp", pathMatch: "prefix"},
-  {path: "githubapp", component: RestappComponent}
+  {path: "githubapp", component: RestappComponent},
+  {path: "nested", component: AppComponent, children: [
+    {path:"header", component: HeaderComponent},
+    {path:"hello", component: HelloWorldComponent}
+  ]}
 ];
 
 @NgModule({
